a975cf709fffc67edee4522f4e77ed57b83e0b5e
Man Phan <phandinhman@gmail.com>
2016-08-02 14:41:12 +0700

    Blog system

---
+++ app/models/entry.rb
@@ -0,0 +1,22 @@
+class Entry < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :body, presence: true
+  validate  :picture_size
+
+  def feed
+    Entry.all
+  end
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+end
